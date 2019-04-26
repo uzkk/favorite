c55f369ae4d8752c17bcda303dde923b6988fe0f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-26 16:24:29 +0000

    Deploy uzkk/favorite to github.com/uzkk/favorite.git:gh-pages

---
+++ assets/js/12.29ef99d2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{106:function(t,e,n){"use strict";n.r(e);var a={name:"Layout"},s=n(0),c=Object(s.a)(a,function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"page section"},[e("Content",{key:this.$page.path,staticClass:"page-content"})],1)},[],!1,null,null,null);e.default=c.exports}}]);
